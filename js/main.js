6425d9fbe4ade5c904ccc1d9b9b439e638245f03
Hebi Li <lihebi.ga@gmail.com>
2014-12-12 23:25:32 -0600

    complete structure

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
-  console.log('hello');
   d=[];
   $('.post-list li a').each(function(index) {
     d.push({
       'href': $(this).attr('href'),
       'title': $(this).text().trim()
     });
+  });
+  $('.page-content').append('<div id="cat"></div>');
+  d.forEach(function(v) {
+    add($('#cat'), v.href, v.title, v.href.substr(7));
+  });
+  $('#cat cat').each(function() {
+    $(this).append('<label>'+$(this).attr('id').substr(4)+'</label>')
   })
-  console.log(d);
 })
 
 function add(ele, href, title, currentHref) {
-  var tmp = currentHref.split('/')[0];
-  if (ele.has('cat#cat-'+tmp)) {
-    add(ele.find('cat#cat-'+tmp), href, title, currentHref.substr(currentHref.indexOf('/')));
-  } else {
-    ele.append($('<cat id="cat-'+tmp+'"></cat>'));
+  if (currentHref.indexOf('/')<0) {
+    ele.append($('<a href="'+href+'">'+title+'</a>'));
+    return;
   }
+  var tmp = currentHref.split('/')[0];
+  var id = 'cat-'+tmp;
+  if (ele.has('cat#'+id).length == 0) {
+    ele.append($('<cat id="'+id+'"></cat>'));
+  }
+  add(ele.find('cat#'+id), href, title, currentHref.substr(currentHref.indexOf('/')+1));
 }
